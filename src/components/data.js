6393b6e015b9df8dea3392a4171aff8b853969f1
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-12-14 23:27:45 +0530

    files cleaned

---
+++ src/components/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    header: "Accordion Item #1",
+    content: "Content for item 1",
+  },
+  {
+    header: "Accordion Item #2",
+    content: "Content for item 2",
+  },
+  {
+    header: "Accordion Item #3",
+    content: "Content for item 3",
+  },
+];
